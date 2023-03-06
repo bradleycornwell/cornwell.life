8353ccd76d8e75c09d5d292c5fb90fb006591779
Bradley Cornwell <bradley@isfluent.com>
2023-03-05 21:09:44 -0500

    bold the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Header() {
 	return (
 		<header className="bg-green-100 flex place-content-between border-b-2 border-green-300 px-4 md:px-8">
-			<h1 className="text-2xl md:text-4xl my-auto text-grey-500">
+			<h1 className="font-bold text-2xl md:text-4xl my-auto text-grey-500">
 				Cornwells in Boston
 			</h1>
 			<Image
